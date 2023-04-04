46fab0f06b34f39a6a6304263039e07eb1a841c6
Bora Göral <B.Goral@student.tudelft.nl>
2023-04-05 01:59:27 +0200

    opponent model added

--- agents/group62_agent/utils/opponent_model.py
+++ agents/group62_agent/utils/opponent_model.py
@@ -1,121 +1,61 @@
-from collections import defaultdict
-
 from geniusweb.issuevalue.Bid import Bid
-from geniusweb.issuevalue.DiscreteValueSet import DiscreteValueSet
 from geniusweb.issuevalue.Domain import Domain
-from geniusweb.issuevalue.Value import Value
+import numpy as np
 
-
+# agent creates an opponent model using frequency modeling
 class OpponentModel:
     def __init__(self, domain: Domain):
-        self.offers = []
-        self.domain = domain
+        # we do not use the offers, we just need the frequencies
+        #self.offers = []
+        self._domain = domain
+        # stores frequencies of offers made by the opponent
+        self._freqs = {}
 
-        self.issue_estimators = {
-            i: IssueEstimator(v) for i, v in domain.getIssuesValues().items()
-        }
+        # template code
+        #self.issue_estimators = {
+        #    i: IssueEstimator(v) for i, v in domain.getIssuesValues().items()
+        #}
+
+        for i in self._domain.getIssues():
+            self._freqs[i] = {}
 
     def update(self, bid: Bid):
-        # keep track of all bids received
-        self.offers.append(bid)
-
-        # update all issue estimators with the value that is offered for that issue
-        for issue_id, issue_estimator in self.issue_estimators.items():
-            issue_estimator.update(bid.getValue(issue_id))
-
-    def get_predicted_utility(self, bid: Bid):
-        if len(self.offers) == 0 or bid is None:
-            return 0
-
-        # initiate
-        total_issue_weight = 0.0
-        value_utilities = []
-        issue_weights = []
-
-        for issue_id, issue_estimator in self.issue_estimators.items():
-            # get the value that is set for this issue in the bid
-            value: Value = bid.getValue(issue_id)
-
-            # collect both the predicted weight for the issue and
-            # predicted utility of the value within this issue
-            value_utilities.append(issue_estimator.get_value_utility(value))
-            issue_weights.append(issue_estimator.weight)
-
-            total_issue_weight += issue_estimator.weight
-
-        # normalise the issue weights such that the sum is 1.0
-        if total_issue_weight == 0.0:
-            issue_weights = [1 / len(issue_weights) for _ in issue_weights]
-        else:
-            issue_weights = [iw / total_issue_weight for iw in issue_weights]
-
-        # calculate predicted utility by multiplying all value utilities with their issue weight
-        predicted_utility = sum(
-            [iw * vu for iw, vu in zip(issue_weights, value_utilities)]
-        )
-
-        return predicted_utility
+        issues = self._domain.getIssues()
+        for i in issues:
+            val = bid.getValue(i)
+            if val not in self._freqs[i]:
+                self._freqs[i][val] = 0
+            self.freqs[i][val] += 1
 
 
-class IssueEstimator:
-    def __init__(self, value_set: DiscreteValueSet):
-        if not isinstance(value_set, DiscreteValueSet):
-            raise TypeError(
-                "This issue estimator only supports issues with discrete values"
-            )
+    def predict_utility(self, bid: Bid):
+        max_frequencies = {}
+        # get utility weights for each issue, with highest-frequency of most common bid values
+        for i in self._domain.getIssues():
+            max_frequencies[i] = 0
+            # check for max
+            for val in self._freqs[i]:
+                if max_frequencies[i] < self._freqs[i][val]:
+                    max_frequencies[i] = self._freqs[i][val]
 
-        self.bids_received = 0
-        self.max_value_count = 0
-        self.num_values = value_set.size()
-        self.value_trackers = defaultdict(ValueEstimator)
-        self.weight = 0
-
-    def update(self, value: Value):
-        self.bids_received += 1
-
-        # get the value tracker of the value that is offered
-        value_tracker = self.value_trackers[value]
-
-        # register that this value was offered
-        value_tracker.update()
-
-        # update the count of the most common offered value
-        self.max_value_count = max([value_tracker.count, self.max_value_count])
-
-        # update predicted issue weight
-        # the intuition here is that if the values of the receiverd offers spread out over all
-        # possible values, then this issue is likely not important to the opponent (weight == 0.0).
-        # If all received offers proposed the same value for this issue,
-        # then the predicted issue weight == 1.0
-        equal_shares = self.bids_received / self.num_values
-        self.weight = (self.max_value_count - equal_shares) / (
-            self.bids_received - equal_shares
-        )
-
-        # recalculate all value utilities
-        for value_tracker in self.value_trackers.values():
-            value_tracker.recalculate_utility(self.max_value_count, self.weight)
-
-    def get_value_utility(self, value: Value):
-        if value in self.value_trackers:
-            return self.value_trackers[value].utility
-
-        return 0
+        highest_freq = max(max_frequencies)
+        util_weights = {}
+        # normalize weights
+        for i in self._domain.getIssues():
+            # avoid div by 0
+            if highest_freq == 0:
+                util_weights[i] = 0
+            else:
+                util_weights[i] = max_frequencies[i] / highest_freq
 
 
-class ValueEstimator:
-    def __init__(self):
-        self.count = 0
-        self.utility = 0
 
-    def update(self):
-        self.count += 1
+        # frequency normalization and multiply with weights:
+        normalised_freqs = []
+        for i in self._domain.getIssues():
+            val = bid.getValue(i)
+            if val in self._freqs[i]:
+                normalised_freqs.append((self._freqs[i][val] / max_frequencies[i]) * util_weights[i])
 
-    def recalculate_utility(self, max_value_count: int, weight: float):
-        if weight < 1:
-            mod_value_count = ((self.count + 1) ** (1 - weight)) - 1
-            mod_max_value_count = ((max_value_count + 1) ** (1 - weight)) - 1
-
-            self.utility = mod_value_count / mod_max_value_count
-        else:
-            self.utility = 1
+        utility_pred = np.sum(normalised_freqs) / len(self._domain.getIssues())
+        return utility_pred
