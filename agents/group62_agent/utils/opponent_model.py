6631342b8bb13f560ba8f1b87a357ab16ea77310
Bora Göral <B.Goral@student.tudelft.nl>
2023-04-09 19:14:01 +0200

    decay over time added for opponent modeling

--- agents/group62_agent/utils/opponent_model.py
+++ agents/group62_agent/utils/opponent_model.py
@@ -1,6 +1,9 @@
 from geniusweb.issuevalue.Bid import Bid
 from geniusweb.issuevalue.Domain import Domain
 import numpy as np
+from time import time
+from geniusweb.progress.ProgressTime import ProgressTime
+from geniusweb.progress.ProgressRounds import ProgressRounds
 
 # agent creates an opponent model using frequency modeling
 class OpponentModel:
@@ -10,7 +13,10 @@ class OpponentModel:
         self._domain = domain
         # stores frequencies of offers made by the opponent
         self._freqs = {}
-
+        # controls effect of time in decay over time - can be modified to improve performance
+        self._exp = 0.4
+        # controls if updates have decay over time or not
+        self._decay = True
         # template code
         #self.issue_estimators = {
         #    i: IssueEstimator(v) for i, v in domain.getIssuesValues().items()
@@ -19,14 +25,18 @@ class OpponentModel:
         for i in self._domain.getIssues():
             self._freqs[i] = {}
 
-    def update(self, bid: Bid):
+    def update(self, bid: Bid, progress):
         issues = self._domain.getIssues()
         for i in issues:
             val = bid.getValue(i)
             if val not in self._freqs[i]:
                 self._freqs[i][val] = 0
-            self._freqs[i][val] += 1
-
+            if self._decay:
+                t = progress.get(time() * 1000)
+                inc = 1 * ((1 - t) ** self._exp)
+                self._freqs[i][val] += inc
+            else:
+                self._freqs[i][val] + 1
     def predict_utility(self, bid: Bid):
         max_frequencies = {}
         # get utility weights for each issue, with highest-frequency of most common bid values
